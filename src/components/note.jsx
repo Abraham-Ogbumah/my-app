6cab1f8f2c69d7e5127628586a02e3b23d40b703
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2021-07-29 08:25:43 +0100

    My First React App
    The learning continues. This app was created using react

---
+++ src/components/note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+    return ( <
+        div className = "note" >
+        <
+        h1 > My Note < /h1> <
+        p > Plain Text < /p> < /
+        div >
+    )
+}
+
+export default Note;
