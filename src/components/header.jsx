94d2b14e6488bd8d0866aeb805a8e365578b2cce
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2021-08-02 10:28:01 +0100

    Update header.jsx
    No major change

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 function Heading() {
-    return ( <
-        header > < h1 > My Keeper App < /h1> < /header >
-    )
+    return ( <header> <h1> My Keeper App </h1> </header> )
 }
 
 export default Heading;
