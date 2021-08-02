9481e89be1d7a97f65f090654e839c48a034e45a
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2021-08-02 10:26:59 +0100

    Update app.jsx
    Imported the notes object to App.js

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import notes from "../notes";
 
 
 import Header from "./header";
 import Note from "./note";
 import Footer from "./footer";
 
+
 function App() {
-    return ( <
-        div >
-        <
-        Header / >
-        <
-        Note / >
-        <
-        Footer / >
-        <
-        /div>
+    return ( <div>
+        <Header />
+        {notes.map((noteItem) => (
+            <Note
+                key={noteItem.key}
+                title={noteItem.title}
+                content={noteItem.content}
+            />))}
+        <Footer />
+        </div>
     )
 }
 
